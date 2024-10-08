744d8db24dffcabaa2397b00d7880f938dfd5c6e
DanielCDoran <40theridgeway@gmail.com>
2024-10-08 19:40:56 +0100

    add in test of array with a null animal

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,15 +1,25 @@
 package com.mycompany.app;
 
-
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static com.mycompany.app.FefiTest.*;
 
 public class AppTest {
-    final String[] animalTypes = new String[] {"Cat", "Dog", "Bat", "Frog"}; 
+    final String[] animalTypes = new String[] { "Cat", "Dog", "Bat", "Frog" };
+    final String[] animalTypesBlank = new String[] { "Cat", "", "Dog", "Bat", "Frog" };
+
     public AppTest() {
-      
+    }
+
+    private int howManyNullAnimals(Animal[] animals) {
+        int count = 0;
+        for (Animal animal : animals) {
+            if (animal == null) {
+                count++;
+            }
+        }
+        return count;
     }
 
     @Test
@@ -23,22 +33,34 @@ public class AppTest {
         Animal result = makeAnimal("");
         assertTrue(result == null);
     }
+
     @Test
     public void testFactoryBlankCharactersInName() {
         Animal result = makeAnimal(" ");
         assertTrue(result == null);
     }
+
     @Test
     public void testArraySizeWithIterative() {
-        // final String[] animalTypes = new String[] {"Cat", "Dog", "Bat", "Frog"};
         final Animal[] animals = getAnimalsFromTypes_iterative(animalTypes);
         assertTrue(animals.length == 4);
     }
 
     @Test
-        public void testArraySizeWithFunctional() {
-            // final String[] animalTypes = new String[] {"Cat", "Dog", "Bat", "Frog"};
-            final Animal[] animals = FefiTest.getAnimalsFromTypes_functional(animalTypes);
-            assertTrue(animals.length == 4);
+    public void testArraySizeWithFunctional() {
+        final Animal[] animals = FefiTest.getAnimalsFromTypes_functional(animalTypes);
+        assertTrue(animals.length == 4);
+    }
+
+    @Test
+    public void testArrayHasNullWithIterative() {
+        final Animal[] animals = getAnimalsFromTypes_iterative(animalTypesBlank);
+        assertTrue(howManyNullAnimals(animals) == 1);
+    }
+
+    @Test
+    public void testArrayHasNullWithFunctional() {
+        final Animal[] animals = FefiTest.getAnimalsFromTypes_functional(animalTypesBlank);
+        assertTrue(howManyNullAnimals(animals) == 1);
     }
 }
